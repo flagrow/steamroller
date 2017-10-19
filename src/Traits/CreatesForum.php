9b9ad0b158fd9bca953b00eaef274a158986551f
Daniel Klabbers <daniel@productip.com>
2017-10-19 12:47:15 +0200

    now builds a forum

--- src/Traits/CreatesForum.php
+++ src/Traits/CreatesForum.php
@@ -4,8 +4,10 @@ namespace Flagrow\Steamroller\Traits;
 
 use Flarum\Forum\Server;
 use Flarum\Foundation\Application;
+use Flarum\Install\Console\DataProviderInterface;
 use Flarum\Install\Console\DefaultsDataProvider;
 use Flarum\Install\Console\InstallCommand;
+use Flarum\Install\InstallServiceProvider;
 use Symfony\Component\Console\Input\StringInput;
 use Symfony\Component\Console\Output\StreamOutput;
 
@@ -27,11 +29,12 @@ trait CreatesForum
         $this->forum = new Server();
     }
 
-    protected function installsForum()
+    protected function installsForum(DataProviderInterface $data)
     {
+        $this->app->register(InstallServiceProvider::class);
         /** @var InstallCommand $command */
         $command = $this->app->make(InstallCommand::class);
-        $data = new DefaultsDataProvider();
+
         $command->setDataSource($data);
 
         $body = fopen('php://temp', 'wb+');
@@ -45,12 +48,22 @@ trait CreatesForum
     {
         $this->createsForumServer();
 
+        $data = new DefaultsDataProvider();
+
+        $database = $data->getDatabaseConfiguration();
+        $database['password'] = '';
+        $database['charset'] = 'utf8mb4';
+        $database['collation'] = 'utf8mb4_unicode_ci';
+
+        $data->setDatabaseConfiguration($database);
+
         $this->forum->setConfig([
-            'url' => 'http://localhost',
-            'debug' => true
+            'url' => $data->getBaseUrl(),
+            'debug' => true,
+            'database' => $data->getDatabaseConfiguration()
         ]);
 
         $this->app = $this->forum->getApp();
-        $this->installsForum();
+        $this->installsForum($data);
     }
 }
